0cb17c09295b40b41d95fbeeef2a7067dc6a697a
NanoThecnolog <contato@ericssongomes.com>
2026-07-18 18:22:40 -0300

    unblock

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ import BlockedPage from "@/components/ui/Blocked";
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false)
-  const isBlocked = true
+  const isBlocked = false
   return (
     <>
       <Head>
