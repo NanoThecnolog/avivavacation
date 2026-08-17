3dcd5923dd0028c48d7c237cac1eace436705763
NanoThecnolog <contato@ericssongomes.com>
2026-08-17 20:08:09 -0300

    block

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,28 +15,34 @@ import WhatsappButton from "@/components/ui/WhatsappButton";
 import Header from "@/components/ui/Header";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import 'swiper/css'
-import 'swiper/css/pagination'
-import 'swiper/css/navigation'
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import BannerSec from "@/components/Banner-Sec";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import BlockedPage from "@/components/ui/Blocked";
 
 export default function Home() {
-  const [isOpen, setIsOpen] = useState(false)
-  const isBlocked = false
+  const [isOpen, setIsOpen] = useState(false);
+  const isBlocked = true;
   return (
     <>
       <Head>
         <title>Clube de Férias Aviva Vacation Clube</title>
-        <meta name="description" content="Clube de Férias Aviva Vacation Clube" />
+        <meta
+          name="description"
+          content="Clube de Férias Aviva Vacation Clube"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="keywords"
           content="clube de férias, clube, férias, vacation, aviva, viagem, planejar férias"
         />
 
-        <meta property="og:title" content="Clube de Férias Aviva Vacation Clube" />
+        <meta
+          property="og:title"
+          content="Clube de Férias Aviva Vacation Clube"
+        />
         <meta
           property="og:description"
           content="Clube de Férias Aviva Vacation Clube."
@@ -49,58 +55,49 @@ export default function Home() {
           content={`${contato.website}/banner-principal.jpg`}
         />
 
-        <meta
-          property="og:url"
-          content={contato.website}
-        />
+        <meta property="og:url" content={contato.website} />
 
         <meta name="robots" content="index, follow" />
 
-        <link
-          rel="canonical"
-          href={contato.website}
-        />
+        <link rel="canonical" href={contato.website} />
         <link rel="icon" href="/favico.svg" />
       </Head>
       <main className={styles.main}>
-        {
-          isBlocked
-            ? <BlockedPage />
-            : <>
-              <Header />
-              <Modal
-                isOpen={isOpen}
-                onClose={() => setIsOpen(false)}
-              />
-              <Swiper
-                className={styles.swiper}
-                modules={[Autoplay, Pagination, Navigation]}
-                slidesPerView={1}
-                loop
-                pagination={{ clickable: true }}
-                //navigation
-                autoplay={{
-                  delay: 6000,
-                  disableOnInteraction: false
-                }}
-              >
-                <SwiperSlide className={styles.slide} data-swiper-autoplay="8000">
-                  <BannerSec openModal={() => setIsOpen(true)} />
-                </SwiperSlide>
-                <SwiperSlide className={styles.slide} data-swiper-autoplay="4000">
-                  <Banner openModal={() => setIsOpen(true)} />
-                </SwiperSlide>
-              </Swiper>
-              <SecondSection />
-              <ThirdSection openModal={() => setIsOpen(true)} />
-              <ForthSection openModal={() => setIsOpen(true)} />
-              <FifthSection />
-              <SixSection openModal={() => setIsOpen(true)} />
-              <SevenSection openModal={() => setIsOpen(true)} />
-              <EightSection openModal={() => setIsOpen(true)} />
-              <WhatsappButton />
-            </>
-        }
+        {isBlocked ? (
+          <BlockedPage />
+        ) : (
+          <>
+            <Header />
+            <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} />
+            <Swiper
+              className={styles.swiper}
+              modules={[Autoplay, Pagination, Navigation]}
+              slidesPerView={1}
+              loop
+              pagination={{ clickable: true }}
+              //navigation
+              autoplay={{
+                delay: 6000,
+                disableOnInteraction: false,
+              }}
+            >
+              <SwiperSlide className={styles.slide} data-swiper-autoplay="8000">
+                <BannerSec openModal={() => setIsOpen(true)} />
+              </SwiperSlide>
+              <SwiperSlide className={styles.slide} data-swiper-autoplay="4000">
+                <Banner openModal={() => setIsOpen(true)} />
+              </SwiperSlide>
+            </Swiper>
+            <SecondSection />
+            <ThirdSection openModal={() => setIsOpen(true)} />
+            <ForthSection openModal={() => setIsOpen(true)} />
+            <FifthSection />
+            <SixSection openModal={() => setIsOpen(true)} />
+            <SevenSection openModal={() => setIsOpen(true)} />
+            <EightSection openModal={() => setIsOpen(true)} />
+            <WhatsappButton />
+          </>
+        )}
       </main>
     </>
   );
